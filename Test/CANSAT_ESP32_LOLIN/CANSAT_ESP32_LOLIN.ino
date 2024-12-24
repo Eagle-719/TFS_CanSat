5076b143852fce302704ba292b531c24a67d56bc
pkarcsi55 <52326567+pkarcsi55@users.noreply.github.com>
2024-12-24 11:23:02 +0100

    Add files via upload
    Ez már egy tökéletes modell! ASC-ben küldi az adatokat és a vevő C#-ban fogadja aza adást

--- Test/CANSAT_ESP32_LOLIN/CANSAT_ESP32_LOLIN.ino
+++ Test/CANSAT_ESP32_LOLIN/CANSAT_ESP32_LOLIN.ino
@@ -7,12 +7,15 @@ Adafruit_BMP280 bmp; // Létrehozzuk a BMP280 példányt I2C BMP SCL ->ESP 23, B
 #define RXD2 16
 #define TXD2 17
 int N=0;
-
+String V = "";
+String H = "";
 
 void setup() {
 Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
 delay(200);
 Serial2.println("sys reset");
+delay(500);
+Serial2.println("radio set pwr 20");//max teljesítmény az adáshoz
 pinMode(LED_BUILTIN, OUTPUT);
 digitalWrite(LED_BUILTIN, LOW);
 Serial.begin(115200);
@@ -25,28 +28,31 @@ Wire.begin();
 }
 
 void loop() {
-  double t=bmp.readTemperature()*100;
-  double p=bmp.readPressure();
-  int ti=(int)t;
-  int pi=(int)p;
-  String T=String(ti);
-  String P=String(pi);
-
-  String S="radio tx " + T + " 1";
-  Serial.println(S);
-  Serial2.println(S);
-   while (Serial2.available()) {
-    Serial.print(char(Serial2.read()));
-   }
-  delay(1000);
-  S="radio tx " + P + " 1";
-  Serial.println(S);
-  Serial2.println(S);
-   while (Serial2.available()) {
-    Serial.print(char(Serial2.read()));
-   }
-
-delay(1000);
+float t = bmp.readTemperature();
+float p = bmp.readPressure();
+String ADATOK="";
+V="TFS" + String(N);conv();ADATOK =ADATOK + H;//Adásazonosító
+V=String(t);conv();ADATOK =ADATOK + H;//Hőmérséklet 24.25 celsiusba
+V=String(p);conv();ADATOK =ADATOK + H;//Nyomás Pa -ban
 N++;
-
+Serial.println(ADATOK);
+String S="radio tx " + ADATOK + " 1";
+Serial2.println(S);
+while (Serial2.available()) {
+  Serial.print(char(Serial2.read()));
+}
+ 
+delay(2000);
+}
+//**************************************************
+void conv(){
+int n = V.length();
+H = "";
+  for (int i = 0; i < n; i++) {
+    String D = V.substring(i, i + 1);
+    char d = D.charAt(0); // Karakter ASCII kódjának kivétele
+    String h = String((int)d, HEX); // ASCII kód hexadecimális konvertálása
+    H = H + h;
+  }
+H=H + "3B";//; szeparátor karakter hozzáadása
 }
